653b2975c103bcb3cf455d23ef9a8fffb873e384
Amadou SANGARE <amadousangare21@gmail.com>
2020-12-04 18:49:27 +0100

    Initialisation du devoir

---
+++ minic.ml
@@ -0,0 +1,34 @@
+type expr =
+    | Cst  of int
+    | Add  of expr * expr
+    | Mul  of expr * expr
+    | Lt   of expr * expr
+    | Get  of string
+    | Call of string * expr list
+    
+type instr =
+    | Putchar of expr
+    | Set     of string * expr
+    | If      of expr * seq * seq
+    | While   of expr * seq
+    | Return  of expr
+    | Expr    of expr
+  and seq = instr list
+
+type typ =
+    | Int
+    | Bool
+    | Void
+
+type fun_def = {
+    name:   string;
+    params: (string * typ) list;
+    return: typ;
+    locals: (string * typ) list;
+    code:   seq;
+  }
+
+type prog = {
+    globals:   (string * typ) list;
+    functions: fun_def list;
+  }
